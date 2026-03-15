bb01a55ef0f5c4dd2bc60af49e15abdb55251848
xor-shift <uygarb@protonmail.com>
2026-03-15 16:10:26 +0100

    tsan was complaining

--- src/log/timers/StdTimer.zig
+++ src/log/timers/StdTimer.zig
@@ -16,11 +16,19 @@ pub fn init() Self {
 
 fn vReset(timer: *Timer) void {
     const self: *Self = @alignCast(@fieldParentPtr("timer", timer));
+
+    self.mutex.lock();
+    defer self.mutex.unlock();
+
     self.std_timer.reset();
 }
 
 fn vRead(timer: *Timer) u64 {
     const self: *Self = @alignCast(@fieldParentPtr("timer", timer));
+
+    self.mutex.lock();
+    defer self.mutex.unlock();
+
     return self.std_timer.read();
 }
 
@@ -29,4 +37,5 @@ timer: Timer = .{ .vtable = &.{
     .read = &Self.vRead,
 } },
 
+mutex: std.Thread.Mutex = .{},
 std_timer: std.time.Timer,
